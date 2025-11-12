b10e43edad04937ea7b5e01fcc49a0d57346c6f9
Yokai47 <gameryounes555@gmail.com>
2025-11-12 20:52:28 +0100

    build: deploy update

---
+++ assets/label--HZwRRLl.js
@@ -0,0 +1,11 @@
+import{c as l,r,j as s,b as n,f,m as c}from"./index-B87UHt5k.js";/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=l("Mail",[["rect",{width:"20",height:"16",x:"2",y:"4",rx:"2",key:"18n3k1"}],["path",{d:"m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7",key:"1ocrg3"}]]);/**
+ * @license lucide-react v0.462.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=l("MessageSquare",[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]]),b=r.forwardRef(({className:a,type:t,...e},o)=>s.jsx("input",{type:t,className:n("flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",a),ref:o,...e}));b.displayName="Input";var m="Label",d=r.forwardRef((a,t)=>s.jsx(f.label,{...a,ref:t,onMouseDown:e=>{var i;e.target.closest("button, input, select, textarea")||((i=a.onMouseDown)==null||i.call(a,e),!e.defaultPrevented&&e.detail>1&&e.preventDefault())}}));d.displayName=m;var u=d;const g=c("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),x=r.forwardRef(({className:a,...t},e)=>s.jsx(u,{ref:e,className:n(g(),a),...t}));x.displayName=u.displayName;export{b as I,x as L,y as M,w as a};
